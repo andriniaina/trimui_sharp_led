59b88bb90f663588afd6cc68c9ae1da28e2c3f04
Andri <andriniaina@gmail.com>
2025-01-30 16:00:24 +0100

    move code to src

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # build
 docker build --pull -t cross-build-arm64 -f Dockerfile.cross-build-x64-arm64 .
-docker run --rm -v $(pwd):/source -w /source cross-build-arm64 dotnet publish -a arm64 -c Release -o bin-arm64 -p:SysRoot=/crossrootfs/arm64 -p:LinkerFlavor=lld
+docker run --rm -v $(pwd)/src/service:/source -w /source cross-build-arm64 dotnet publish -a arm64 -c Release -o bin-arm64 -p:SysRoot=/crossrootfs/arm64 -p:LinkerFlavor=lld
 ls bin-arm64
 
 # sanity test
